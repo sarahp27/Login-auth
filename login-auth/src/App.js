d7dfb3bbcadf5407034a1f5404445390428dd6bc
muhammadhuzaifa <huzaifa.arshad@xloopdigital.com>
2023-05-03 15:58:20 +0500

    auth work

--- login-auth/src/App.js
+++ login-auth/src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <Homepage/>
+      <center>
       <Login />
+      </center>
 
     </div>
   );
--- login-auth/src/Components/Login.jsx
+++ login-auth/src/Components/Login.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from "react";
 import './style.css'
+import styles from '../modules/login.module.css'
+import user from '../utils/user.info.json'
 
 function Login() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
 
+  const emailInput = (e) => {
+    setEmail(e.target.value)
+    console.log(email)
+  }
+
+  const passwordInput = (e) => {
+    setPass(e.target.value)
+    console.log(pass)
+  }
+
+  const submit = (e) =>{
+    console.log(user)
+    e.preventDefault()
+    if(user.email === email && user.password === pass){
+      console.log("loged in")
+      localStorage.setItem("user", JSON.stringify(user))
+    }
+  }
+
   return (
     <div className="container">
-      <div className="wrapper">
-        <label>
-          Email
-          <input type="text" name={email} />
-        </label>
-        <label>
-          Password:
-        <input type="text" name={email} />
-        </label>
-
-        <button className="btn">Login</button> 
+      <form onSubmit={(e) => submit(e)}>
+      <div class={`mb-3 ${styles.fields}`}>
+        <label for="exampleInputEmail1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" onChange={(e) => emailInput(e)} />
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
+      <div class={`mb-3 ${styles.fields}`}>
+        <label for="exampleInputPassword1" class="form-label">Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" onChange={(e) => passwordInput(e)}/>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
-    
   );
 }
 
