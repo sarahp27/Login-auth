258d7c0d5542db66ecaacf21575c8da80694b7be
sarahp27 <sarah.patel@xloopdigital.com>
2023-05-03 15:26:23 +0500

    homepage updated

--- login-auth/src/Components/Homepage.jsx
+++ login-auth/src/Components/Homepage.jsx
@@ -1,19 +1,35 @@
-import React from 'react'
-import NavBar from './NavBar'
-import './style.css'
+import React from "react";
+import NavBar from "./NavBar";
+import "./style.css";
 
 function Homepage() {
   return (
     <>
-   <NavBar/>
+      <NavBar />
 
-   <div className='banner'>
-    <h1 className='head'> Cyber Security </h1>
-    <p className='para'>The practice of defending computers, servers, mobile devices, electronic systems
-      <br/>, networks, and data from malicious attacks. </p>
-   </div>
-   </>
-  )
+      <div className="banner">
+        <h1 className="head"> Cyber Security </h1>
+        <p className="para">
+          The practice of defending computers, servers, mobile devices,
+          electronic systems,
+          <br />
+          networks, and data from malicious attacks.{" "}
+        </p>
+      </div>
+
+      <div className="material">
+        <div className="info">
+          <h2>Why is Cyber Security Important?</h2>
+          <ul>
+            <li>It costs of cyber security breaches are rising.</li>
+            <li>Cyber attacks are increasingly sophisticated.</li>
+            <li>Cyber security is a critical, board-level issue.</li>
+            <li>Cyber crime is a big business.</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Homepage
+export default Homepage;
